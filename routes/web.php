07c1a32100f47a830517214fa0f6673110c54a81
Sanechika <nobuki.1127.s@gmail.com>
2022-02-13 11:16:31 +0900

    [PHP/Laravel 11 – [Mac編]ControllerとViewが連携できるようにしよう]

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add');
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+
+    Route::get('profile/create', 'Admin\ProfileController@add');
+    Route::get('profile/edit', 'Admin\ProfileController@edit');
 });
 
 // http://XXXXXX.jp/XXX/AAAContoroller/bbb
 Route::get('XXX', 'AAAController@bbb');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
