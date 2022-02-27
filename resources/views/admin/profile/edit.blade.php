b9e96f8eb75f9fcbd970904950f429bb5a9e76c6
Sanechika <nobuki.1127.s@gmail.com>
2022-02-27 11:07:31 +0900

    PHP/Laravel11　課題

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -1,13 +1,13 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.admin')
 
-        <title>profile</title>
-    </head>
-    <body>
-        <h1>profile編集画面</h1>
-    </body>
-</html>
+@section('title', 'profile編集')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>profile編集</h2>
+            </div>
+        </div>
+    </div>
+@endsection
